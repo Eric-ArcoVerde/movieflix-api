c8f007f6cb4279d5521f9c59974bc6b341986077
Eric Barros Arco-Verde <eric.arcoverde@gmail.com>
2026-04-07 21:36:22 -0300

    adicionada correção de flmes duplicados

--- src/server.ts
+++ src/server.ts
@@ -1,40 +1,52 @@
-import express from "express";
-import { PrismaClient } from "./generated/prisma/index.js";
+import express from 'express'
+import { PrismaClient } from './generated/prisma/index.js'
 
-const port = 3000;
-const app = express();
-const prisma = new PrismaClient();
+const port = 3000
+const app = express()
+const prisma = new PrismaClient()
 
-app.use(express.json());
+app.use(express.json())
 
 app.get('/movies', async (_, res) => {
- const movies = await prisma.movie.findMany({
-   orderBy: { title: 'asc' },
-   include: { genres: true, languages: true },
- });
- res.json(movies);
-});  
+    const movies = await prisma.movie.findMany({
+        orderBy: { title: 'asc' },
+        include: { genres: true, languages: true },
+    })
+    res.json(movies)
+})
 
 app.post('/movies', async (req, res) => {
+    const { title, genre_id, language_id, oscar_count, release_date } = req.body
 
- const { title, genre_id, language_id, oscar_count, release_date } = req.body;
+    try {
+        const movieWithSameTitle = await prisma.movie.findFirst({
+            where: {
+                title: { equals: title, mode: 'insensitive' },
+            },
+        })
 
- try{
- await prisma.movie.create({
-   data: {
-     title,
-     genre_id,
-     language_id,
-     oscar_count,
-     release_date: new Date(release_date),
-   },
- });
- } catch(error){
-   return res.status(500).send({message: "Falha ao cadastrar o filme."});
- }
- res.status(201).send();
-});
+        if (movieWithSameTitle) {
+            return res
+                .status(409)
+                .send({ message: 'Já existe um filme com esse título' })
+        }
+
+        await prisma.movie.create({
+            data: {
+                title,
+                genre_id,
+                language_id,
+                oscar_count,
+                release_date: new Date(release_date),
+            },
+        })
+    } catch (error) {
+        return res.status(500).send({ message: 'Falha ao cadastrar um filme' })
+    }
+
+    res.status(201).send()
+})
 
 app.listen(port, () => {
-   console.log(`Servidor em execução em http://localhost:${port}`);
-});
+    console.log(`Servidor em execução em http://localhost:${port}`)
+})
